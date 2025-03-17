8c35ee90487e1cf7f5d3af014586dfe09755e82a
Marc Charignon <marco.charignon@gmail.com>
2025-03-17 20:57:30 +0100

    first shell commit

---
+++ Shell/shell.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <stdbool.h>
+#include <sys/stat.h>
+
+char currentpath[200];
+
+bool compare(char* str1, char* str2){
+    // Compare la chaîne str1 au début de la chaîne str2.
+    printf("ATTENTION str1 = %s, str2 = %s ATTENTION\n", str1, str2);
+    int i=0;
+    while(str1[i] != '\0'){
+        printf("str 1 : %d, str2 : %d\n", str1[i], str2[i]);
+        if (str1[i] != str2[i] && ((int)str1[i] != 32 || (int)str2[i] != 0)){
+            //if ((int)str1[i] != 0 || (int)str2[i] != 32){
+            
+            //} 
+            return false;
+        }
+        i += 1; 
+    }
+    return true;
+}
+
+void changeDirectory(char* path){
+    printf("Path : %s\n", path);
+    if (path == NULL){
+        strcpy(currentpath,"/~");
+    } else if(path[0] == '.' && strlen(path) == 1) {
+        printf("Path : BONJOUR %s\n", path);
+        return;
+    } else if(path == "..") {
+        // TODO Si à la racine, ne rien faire, sinon remonter
+        return;
+    } else if(path[0] == '/'){ // Chemin absolu
+        struct stat stats;
+        if (stat(path, &stats)){
+            strcpy(currentpath,path);
+        }
+    } else { // Chemin relatif
+        char* finalpath = malloc(strlen(path) + strlen(currentpath) + 2);
+        strcpy(finalpath,currentpath);
+        strcat(finalpath,"/");
+        strcat(finalpath,path);
+        struct stat stats;
+        if (stat(finalpath, &stats) == 0){
+            strcpy(currentpath,finalpath);
+        }
+        free(finalpath);
+    }
+}
+
+void cutstr(char* str){
+    int i=0;
+    while(str[i] != '\n'){
+        i++;
+    }
+    str[i] = '\0';
+}
+
+int main(int argc, char *argv[]){
+    char entry[200];
+    fgets(entry,sizeof(entry),stdin); // Il faut cut le string au \n
+    cutstr(entry);
+    //scanf("%s",entry); // PROBLEME : S'ARRETE AUX ESPACES
+
+    getcwd(currentpath,200); // récupère le path actuel
+
+    if(compare("cd ",entry)) { // NE MARHCE PAS AVEC CD + RIEN
+        char* start = &entry[3];
+        printf("Réussi : entry = %s\n", start);
+        changeDirectory(start); // IL FAUT COUPER LE CD
+    } else if(compare("./",entry)) {
+        /*Spawn a child to run the program.*/
+        pid_t pid=fork();
+        if (pid==0) { /* child process */
+            char* name = &entry[2];
+            printf("name %s\n", name);
+            char* path = malloc(sizeof(entry) + sizeof(currentpath) + 1);
+            strcpy(path,currentpath);
+            strcat(path,"/");
+            strcat(path,name);
+            char *argv2[]={name,NULL};
+            execv(argv[1],argv2);
+            free(path); // A relocaliser ? 
+            exit(127); /* only if execv fails */
+        }
+        else { /* pid!=0; parent process */
+            waitpid(pid,0,0); /* wait for child to exit */
+        }
+    }
+
+    printf("Le path actuel est %s\n", currentpath);
+    
+    return 0;
+}
