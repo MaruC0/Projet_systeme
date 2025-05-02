5e63f16167fa6503df27ac0e6fcf02f6af0ac284
Alexandre de Montaigne <aldemontaigne@icloud.com>
2025-05-02 12:05:18 +0200

    Update test2.c
    Trop de include là aussi

--- test2.c
+++ test2.c
@@ -1,14 +1,5 @@
-#include <errno.h>
-#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <errno.h>
-#include <fcntl.h>
 
 int main(void){
     printf("STRING DE TEST OMG");
